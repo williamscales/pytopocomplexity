d0dca27f2dc468f831216af215e05a0659d19abb
William Scales <william.scales@zzw.ca>
2014-11-26 15:33:32 -0800

    fixed up setup.py

--- setup.py
+++ setup.py
@@ -12,51 +12,85 @@ import sys
 from setuptools import setup, find_packages
 
 
+def read(file):
+    """ Reads and returns as a string the contents of a file.
+
+    Parameters
+    ----------
+    file : str
+        Path to the file to be read, relative to the directory where setup.py is
+        located
+
+    Returns
+    -----
+    output : str
+        The contents of `file`.
+
+    """
+    here = path.abspath(path.dirname(__file__))
+    with open(path.join(here, file), encoding='utf-8') as f:
+        output = f.read()
+    return output
+
+
 def main(argv=None):
     if argv is None:
         argv = sys.argv[1:]
 
-    here = path.abspath(path.dirname(__file__))
-
-    # Get the long description from the relevant file
-    with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-        long_description = f.read()
+    package_name = 'pytopocomplexity'
+    package_version = '0.1'
+    homepage_url = 'https://github.com/williamscales/pytopocomplexity'
+    package_author = 'William Scales'
+    package_author_email = 'william@wscales.com'
+    license = 'Apache Software License'
+    short_description = 'Algorithms using entropy to estimate complexity of'\
+                        ' high dimensional functions'
+    long_description = read('README.rst')
+    required = [
+        'future',
+        'numpy',
+    ]
+    extras = {
+        'develop': [
+            'sphinx',
+            'pytest'
+        ]
+    }
+    classifiers = [
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Physics'
+    ],
+    keywords = 'complexity, optimization'
 
     setup(
-        name='pytopocomplexity',
-        version='0.1',
-        description='Algorithms using entropy to estimate complexity of high'
-                    'dimensional functions',
+        name=package_name,
+        version=package_version,
+        description=short_description,
         long_description=long_description,
-        url='https://github.com/williamscales/pytopocomplexity',
-        author='William Scales',
-        author_email='william@wscales.com',
-        license='MIT',
+        url=homepage_url,
+        author=package_author,
+        author_email=package_author_email,
+        license=license,
         platforms='OS Independent',
-        classifiers=[
-            'Development Status :: 1 - Planning',
-            'Environment :: Console',
-            'Intended Audience :: Science/Research',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: MIT License',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.3',
-            'Programming Language :: Python :: 3.4',
-            'Topic :: Scientific/Engineering',
-            'Topic :: Scientific/Engineering :: Mathematics',
-            'Topic :: Scientific/Engineering :: Physics'
-        ],
-        keywords='complexity, optimization',
+        classifiers=classifiers,
+        keywords=keywords,
         packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-        install_requires=[
-            'future',
-            'numpy',
-        ],
+        install_requires=required,
+        extras_require=extras,
     )
 
 if __name__ == '__main__':
